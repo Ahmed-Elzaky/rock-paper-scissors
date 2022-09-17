bdc2213c759dc9576b4c50cee1717bdc49221114
Ahmed-Elzaky <A7med3lzeky@outlook.com>
2022-09-17 15:17:10 +0200

    modified javascript.js and add style.css

--- javascript.js
+++ javascript.js
@@ -1,44 +1,41 @@
-console.log("Rock Paper Scissors")
+console.log("Rock Paper Scissors");
 
-let getComputerChoice = function (){
+function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
-  switch (randomNumber){
+  switch (randomNumber) {
     case 0:
       return "rock";
     case 1:
       return "paper";
     case 2:
       return "scissors";
-  };
-};
+  }
+}
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
-  console.log(`Computer: ${computerSelection}`);
-  if(computerSelection === playerSelection)
-    return "Even";
+  if (computerSelection === playerSelection) return "Even";
   else {
-    if(computerSelection === "rock"){
-      if(playerSelection === "paper")
+    if (computerSelection === "rock") {
+      if (playerSelection === "paper")
         return "You Wine! Paper beats Rock";
       else if (playerSelection === "scissors")
         return "You Lose! Rock beats Scissors";
     }
-    if(computerSelection === "paper"){
-      if(playerSelection === "rock")
+    if (computerSelection === "paper") {
+      if (playerSelection === "rock")
         return "You Lose! Paper beats Rock";
-      else if(playerSelection === "scissors")
+      else if (playerSelection === "scissors")
         return "You Wine! Scissors beats Paper";
     }
-    if(computerSelection === "scissors"){
-      if(playerSelection === "rock")
+    if (computerSelection === "scissors") {
+      if (playerSelection === "rock")
         return "You Wine! Rock beats Scissors";
-      else if(playerSelection === "paper")
+      else if (playerSelection === "paper")
         return "You Wine! Scissors beats Paper";
     }
   }
 }
-
 
 function game() {
   for (let i = 0; i < 5; i++) {
@@ -48,5 +45,26 @@ function game() {
   }
 }
 
-game();
+// game();
 
+const para = document.querySelector("p");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  const getPlayerChoice = input.value.toLowerCase();
+  input.value = "";
+  input.focus();
+  para.textContent = "";
+  if (
+    getPlayerChoice === "rock" ||
+    getPlayerChoice === "paper" ||
+    getPlayerChoice === "scissors"
+  ) {
+    // let getComputerChoice = getComputerChoice();
+    // para.textContent =
+    para.textContent = playRound(getPlayerChoice, getComputerChoice());
+  } else {
+    para.textContent = "Wrong choice";
+  }
+});
